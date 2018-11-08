458ddeb2a3e16243e2ed9f5f2a5344725d958bab
shanshan <970926205@qq.com>
2018-11-08 15:08:31 +0800

    Update document/test1108.sql
    Database file

---
+++ document/test1108.sql
@@ -0,0 +1,47 @@
+/*
+SQLyog Ultimate v12.2.6 (32 bit)
+MySQL - 8.0.12 : Database - springboott1105
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`springboott1105` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `springboott1105`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `mender` varchar(10) DEFAULT NULL,
+  `updatedate` timestamp NULL DEFAULT NULL,
+  `creator` varchar(10) DEFAULT NULL,
+  `createdate` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`sex`,`phone`,`email`,`birthday`,`mender`,`updatedate`,`creator`,`createdate`) values 
+(2,'xiaohei','4297F44B13955235245B2497399D7A93',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(3,'xiaohua','4297F44B13955235245B2497399D7A93',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
